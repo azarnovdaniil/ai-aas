4c59496bbeb40f22f07a1db5ef61ce4d1946a846
daniilazarnov <azarnow.d@gmail.com>
2019-01-02 22:42:24 +0300

    fix all dependencies and application property files

--- web/src/main/java/ru/daniilazarnov/web/Client.java
+++ web/src/main/java/ru/daniilazarnov/web/Client.java
@@ -1,7 +1,6 @@
 package ru.daniilazarnov.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
